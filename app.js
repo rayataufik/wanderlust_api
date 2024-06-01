6fa7bb38bc3d01c7086062d9f3ca34a47010c24c
Raya Taufik <rayataufik4@gmail.com>
2024-06-02 01:58:35 +0700

    rollback

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const bodyParser = require('body-parser');
 const itemsRoutes = require('./routes/contents');
 
 const app = express();
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use('/api/contents', itemsRoutes);
 
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});

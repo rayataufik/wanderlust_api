e24a7f6331b16d8e5293ebe4ae427d8cd5dead52
Raya Taufik <rayataufik4@gmail.com>
2024-06-02 02:21:30 +0700

    remove port

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const bodyParser = require('body-parser');
 const itemsRoutes = require('./routes/contents');
 
 const app = express();
-const port = 4000;
 
 app.use(bodyParser.json());
 app.use('/api/contents', itemsRoutes);
 
-app.listen(port, () => {
-  console.log(`API listening on PORT ${port} `)
-})
 
 module.exports = app;

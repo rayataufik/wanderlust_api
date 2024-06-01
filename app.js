d4b1f23bbfb745359a535928d57facac74af0038
Raya Taufik <rayataufik4@gmail.com>
2024-06-02 02:09:18 +0700

    update port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const itemsRoutes = require('./routes/contents');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(bodyParser.json());
 app.use('/api/contents', itemsRoutes);

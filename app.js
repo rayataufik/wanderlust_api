7de23760d5549af4c05d1053c104b6579232a629
Raya Taufik <rayataufik4@gmail.com>
2024-06-02 01:55:31 +0700

    remove port for vercel

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const bodyParser = require('body-parser');
 const itemsRoutes = require('./routes/contents');
 
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
 app.use('/api/contents', itemsRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});

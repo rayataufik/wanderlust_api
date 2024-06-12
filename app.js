e822b338ea104ae783708c331acef67ff8439d22
Raya Taufik <61215395+rayataufik@users.noreply.github.com>
2024-06-13 01:46:59 +0700

    Fix cors

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const itemsRoutes = require('./routes/contents');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // Allow all domains
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+  });
+  
 app.use(bodyParser.json());
 app.use('/api/contents', itemsRoutes);
 
